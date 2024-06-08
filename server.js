582c1c4e7099cc9b609da5689d4a01d5e911e47a
markointihar <47603551+markointihar@users.noreply.github.com>
2024-06-08 12:58:56 +0200

    deploy fix4

--- server.js
+++ server.js
@@ -62,5 +62,9 @@ io.on('connection', socket => {
 })
 
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = (req, res) => {
+  return app(req, res);
+};
